22bb32f5cfc376563d48d1e14f4ad75eb20a9d28
sorokin <sorokin44rus@icloud.com>
2023-12-03 16:46:22 +0300

    Initial Commit

---
+++ CarMoney/CarMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarMoneyApp.swift
+//  CarMoney
+//
+//  Created by  OLEG on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CarMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
